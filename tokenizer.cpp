4482a43fb9b560361466e41ab2bc1939f6ada7a2
Jan Knížek <jk@janknizek.cz>
2018-03-23 09:33:26 +0100

    tokenizing without arguments

--- tokenizer.cpp
+++ tokenizer.cpp
@@ -1,18 +1,22 @@
 // #define DEBUG
+#include <utility>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <map>
 #include <vector>
+#include <map>
 #include <memory>
 #include "tokenizer.hpp"
 
-charGroup processStream::getCurrent() {
+// PROCESS STREAM
+
+charGroup processStream::getCurrent() const {
 	if(eof_) return charGroup::eof;
 
 	switch(curr_) {
 		case ';':
-			return charGroup::terminator;
+			return charGroup::semicolon;
 
 		case '\n':
 		case '\r':
@@ -46,17 +50,18 @@ charGroup processStream::getCurrent() {
 
 	if(('a'<=curr_ && curr_<='z') ||
 		('A'<=curr_ && curr_<='Z') ||
-		('0'<=curr_ && curr_<='9'))
+		('0'<=curr_ && curr_<='9') ||
+		('_' == curr_))
 		return charGroup::alphanum;
 
-	// for(char && c: ops_)  ??
+	// for(char && c: ops_)  ?? TODO
 	for(char c: ops_) {
 		if(c==curr_)
 			return charGroup::op;
 	}
 
 #ifdef DEBUG
-	std::cout<<"INVALID "<<curr_<<std::endl;
+	std::cout<<"INVALID CHARACTER "<<curr_<<" "<<(int)curr_<<std::endl;
 #endif
 	return charGroup::invalid;
 };
@@ -68,13 +73,14 @@ std::string processStream::flush() {
 };
 
 void processStream::move() {
-	in_>>curr_;
+	// in this order, so curr_ is never in the buffer
 	buffer_.push_back(curr_);
-	if(in_.eof())
+	in_>>curr_;
+	if(in_.eof()) // TODO EOF _ is the last character added? No chance
 		eof_=true;
 
 #ifdef DEBUG
-	std::cout<<in_.eof()<<" "<<curr_<<std::endl;
+	std::cout<<"DEBUG: PROCESS STREAM: EOF? "<<in_.eof()<<" "<<curr_<<std::endl;
 #endif
 };
 
@@ -88,31 +94,157 @@ void processStream::moveBack() {
 };
 
 
+// TOKENIZER
+template<charGroup group, token tok>
+token parseOneChar(processStream& in) {
+	if(in.getCurrent()==group)
+		return tok;
+	return token::invalidCharacter;
+}
+
+token parseSymbol(processStream& in) {
+	charGroup group=in.getCurrent();
+	if(group!=charGroup::alphanum &&
+			group!=charGroup::quote &&
+			group!=charGroup::op)
+		return token::invalidCharacter;
+	
+	while(group==charGroup::alphanum ||
+			group==charGroup::quote ||
+			group==charGroup::op) {
+		in.move();
+		group=in.getCurrent();
+	}
+
+	return token::symbol;
+}
+
+// Whitespace (' ', \f, \t, \v), [Newline (\r, \n)] + comments
+// satisfies empty string
+// TODO comments
+template<bool newLines>
+token parseWC(processStream& in) {
+	charGroup group=in.getCurrent();
+	while(group==charGroup::whitespace ||
+			(newLines && group==charGroup::newline)) {
+		in.move();
+		group=in.getCurrent();
+	}
+	return token::skip;
+}
+
+token parseTerminator(processStream& in) {
+	charGroup group=in.getCurrent();
+	if(group==charGroup::semicolon ||
+		group==charGroup::newline) {
+		in.move();
+		return token::terminator;
+	}
+	return token::invalidCharacter;
+}
+
+// checks for ) , or EOF
+token parseBlockEnd(processStream& in) {
+	charGroup group=in.getCurrent();
+	if(group==charGroup::eof ||
+			group==charGroup::comma ||
+			group==charGroup::close)
+		// don't move here - comma or close will be used by the upper layer
+		return token::eof;
+
+	return token::invalidCharacter;
+}
+
+void tokenizer::prepareBoxes() {
+	// prepare boxes
+	// WC1
+	boxes_.insert({"WC1", std::make_unique<tokenBox>(in_, parseWC<false>)});
+	
+	boxes_.insert({"BlockEnd",
+			std::make_unique<tokenBox>(in_, parseBlockEnd)});
+
+	boxes_.insert({"Terminator",
+			std::make_unique<tokenBox>(in_, parseTerminator)});
+	
+	boxes_.insert({"Symbol",
+			std::make_unique<tokenBox>(in_, parseSymbol)}); // todo quote
+	boxes_.insert({"WC2",
+			std::make_unique<tokenBox>(in_, parseWC<false>)});
+	boxes_.insert({"Open",
+			std::make_unique<tokenBox>(in_,
+					parseOneChar<charGroup::open, token::skip>)});
+
+	boxes_.insert({"NWC1",
+			std::make_unique<tokenBox>(in_, parseWC<true>)});
+	boxes_.insert({"Argument",
+			std::make_unique<tokenBox>(in_, parseSymbol)}); // TODO
+	boxes_.insert({"NWC2",
+			std::make_unique<tokenBox>(in_, parseWC<true>)});
+	boxes_.insert({"Comma",
+			std::make_unique<tokenBox>(in_,
+					parseOneChar<charGroup::comma, token::skip>)});
+	boxes_.insert({"Close",
+			std::make_unique<tokenBox>(in_,
+					parseOneChar<charGroup::close, token::skip>)});
+	
+	// transition
+	std::vector<std::pair<std::string, std::string> > transitions={
+											{"WC1", "Terminator"}, 
+											{"Terminator", "WC1"}, 
+
+											{"WC1", "BlockEnd"}, 
+	
+											{"WC1", "Symbol"},
+											{"Symbol", "WC2"},
+											{"WC2", "Open"},
+											{"Open", "NWC1"},
+											{"NWC1", "Argument"},
+											{"Argument", "NWC2"}, // TODO THIS IS WRONG
+											{"NWC2", "Close"},
+											{"Close", "WC1"},
+	
+											{"NWC2", "Comma"},
+											{"WC2", "WC1"}}; // this have lower priority DIRTY TRICK WARNING TODO
+
+
+	for(auto && tr: transitions) {
+		tokenBox* to=boxes_.find(tr.second)
+								->second.get();
+		boxes_.find(tr.first)
+			->second
+			->addNextBox(to);
+	}
+
+	curr_=boxes_.find("WC1")->second.get();
+}
+
+token tokenizer::nextToken() {
+	tokenBox* newCurr=curr_->getNextBox();
+	token currToken=newCurr->parseToken(); // TODO KDYŽ DOJDOU BOXY
+	while(currToken==token::invalidCharacter) {
+		newCurr=curr_->getNextBox();
+		currToken=newCurr->parseToken();
+	}
+
+	curr_=newCurr;
+
+	return currToken;
+}
+
 int main(int argc, char * * argv) {
 	std::ifstream iff { "tests/testfile.io" };
-	processStream in(iff);
+	processStream in(iff); 
 
-	in.move(); std::cout<<in.getCurrent()<<std::endl;
-	in.move(); std::cout<<in.getCurrent()<<std::endl;
-	in.move(); std::cout<<in.getCurrent()<<std::endl;
-	in.move(); std::cout<<in.getCurrent()<<std::endl;
-	std::cout<<in.flush()<<std::endl;
-	in.move(); std::cout<<in.getCurrent()<<std::endl;
-	in.move(); std::cout<<in.getCurrent()<<std::endl;
-	in.move(); std::cout<<in.getCurrent()<<std::endl;
-	in.move(); std::cout<<in.getCurrent()<<std::endl;
-	in.move(); std::cout<<in.getCurrent()<<std::endl;
-	std::cout<<in.flush()<<std::endl;
-	in.move(); std::cout<<in.getCurrent()<<std::endl;
-	in.move(); std::cout<<in.getCurrent()<<std::endl;
-	in.move(); std::cout<<in.getCurrent()<<std::endl;
-	in.move(); std::cout<<in.getCurrent()<<std::endl;
-	in.move(); std::cout<<in.getCurrent()<<std::endl;
-	in.move(); std::cout<<in.getCurrent()<<std::endl;
-	in.move(); std::cout<<in.getCurrent()<<std::endl;
-	in.move(); std::cout<<in.getCurrent()<<std::endl;
-	in.move(); std::cout<<in.getCurrent()<<std::endl;
+	tokenizer run(in);
 
-	std::cout<<in.flush()<<std::endl;
+	std::cout<<(int)run.nextToken()<<' '<<in.flush()<<std::endl;
+
+	token currToken;
+	do {
+		currToken=run.nextToken();
+		if(currToken==token::skip) continue;
+
+		std::cout<<(int)currToken<<' '<<in.flush()<<std::endl;
+	} while(currToken!=token::eof);
 	return 0;
 }
--- tokenizer.hpp
+++ tokenizer.hpp
@@ -3,15 +3,21 @@
 
 #include <istream>
 #include <string>
+#include <vector>
+#include <memory>
+#include <map>
 
 #ifdef DEBUG
 #include <iostream>
 #endif
 
-enum charGroup {
+
+// STREAM
+
+enum class charGroup {
 	// value is ASCII of one of the representants
 	alphanum,
-	terminator,
+	semicolon,
 	newline,
 	whitespace,
 	op, // operator - does not include slash '/'
@@ -25,16 +31,24 @@ enum charGroup {
 	invalid
 };
 
+// after construct - getCurrent gives the first char
 class processStream {
 	public:
 		processStream(std::istream & in): in_(in) {
-			in_>>std::noskipws;
+			in_>>std::noskipws>>curr_;
+
+#ifdef DEBUG
+			std::cout<<"DEBUG: PROCESS STREAM: EOF? "<<in_.eof()<<" "<<curr_<<std::endl;
+#endif
 		};
 
-		charGroup getCurrent();
+		charGroup getCurrent() const;
 		void move();
 		void moveBack();
 		std::string flush();
+		bool eof() {
+			return eof_;
+		}
 
 	private:
 		std::istream & in_;
@@ -46,6 +60,78 @@ class processStream {
 			'[', ']', ',', '<', '>', '?'};
 
 		std::string buffer_;
+};
+
+
+// BOXES
+enum class token {
+	symbol,				// 0
+	skip, 				// 1
+	argument,			// 2
+	terminator,			// 3
+	eof,				// 4
+	endOfArgument,		// 5
+	invalidCharacter	// 6
+};
+
+using parseTokenSignature = token (*) (processStream &);
+
+class tokenBox {
+	public:
+		tokenBox(processStream& in, parseTokenSignature parseTokenFunc):
+			in_(in),
+			parseToken_(parseTokenFunc) {
+			// nextBoxes_=std::vector<tokenBox>();
+		};
+
+		void addNextBox(tokenBox* box) {
+			nextBoxes_.push_back(box);
+		};
+
+		tokenBox* getNextBox() {
+			// if(indexNextBox // TODO check boundaries
+			return nextBoxes_[indexNextBox_++];
+		};
+
+		token parseToken() {
+			indexNextBox_=0; // offer nextBoxes from start again
+			return parseToken_(in_);
+		}
+
+	private:
+		std::vector<tokenBox* > nextBoxes_;
+		processStream& in_;
+		std::size_t indexNextBox_=0;
+		parseTokenSignature parseToken_;
+};
+
+
+// tokenizer - returns token by token
+// Boxflow
+//        |---> EOF
+//        |
+// WC1 -->+---> Terminator --> back to WC1
+//        |
+//        |---> Symbol -> WC2 -+-> Open -- NWC1 -> Argument -> NWC2 --+-> Close --+
+//                             |         ^                            |           |
+//                     WC1 <---+         +-<---------- Comma <--------+    WC1 <--+
+// Next BOX is always unambiguously defined - not true!! TODO
+
+class tokenizer {
+	public:
+		tokenizer(processStream& in): in_(in) {
+			prepareBoxes();
+		};
+
+		void prepareBoxes();
+
+		token nextToken();
+
+	private:
+		tokenBox* curr_; // there is last used box
+		std::map<std::string, std::unique_ptr<tokenBox> > boxes_;
+		processStream& in_;
+
 };
 
 #endif
